b44ec77860971ed156caf662da41b972ba341fdb
zaeemkhan <khanzaeem010@gmail.com>
2022-08-16 14:27:02 +0500

    Add project files.

---
+++ ImmigrationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImmigrationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
